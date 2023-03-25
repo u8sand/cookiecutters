29a0189abc340156471e9ef77b5f49aee0a31187
Daniel Clarke <u8sand@gmail.com>
2023-03-25 13:28:15 -0400

    Add cookiecutters for configuring systemd-networkd

---
+++ systemd-network-link/hooks/post_gen_project.py
@@ -0,0 +1,14 @@
+import pathlib
+
+cookiecutter_path = pathlib.Path("{{ cookiecutter.filename }}.link").absolute()
+systemd_networkd_path = pathlib.Path("/etc/systemd/network") / cookiecutter_path.name
+print(f"Move {cookiecutter_path.name} => {systemd_networkd_path}{' (overwrite)' if systemd_networkd_path.exists() else ''}? [y/N]")
+if input().lower() == 'y':
+  systemd_networkd_path.parent.mkdir(parents=True, exist_ok=True)
+  cookiecutter_path.rename(systemd_networkd_path)
+else:
+  cookiecutter_path.rename(
+    cookiecutter_path.parent.parent / cookiecutter_path.name
+  )
+
+cookiecutter_path.parent.rmdir()
